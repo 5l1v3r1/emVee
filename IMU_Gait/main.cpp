379e587ba7a2dc1b7a7013b3f4f12da14875edb3
William Chavez-Salinas <vvilliann@Williams-MacBook-Pro.local>
2015-02-08 15:14:47 -0500

    Initial Commit

---
+++ IMU_Gait/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IMU_Gait
+//
+//  Created by William Chavez-Salinas on 2/8/15.
+//  Copyright (c) 2015 William Chavez-Salinas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
